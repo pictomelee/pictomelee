058af9ee462b2a24d0d72d1b75194d4613512320
Rempee Kalia <Rempee Kalia>
2016-04-02 19:26:24 -0400

    when image is clicked, another one is up

---
+++ pictureclick.py
@@ -0,0 +1,28 @@
+"When clicked, will print something, after will print out three images"
+
+import sys
+from clarifai.client import ClarifaiApi
+
+clarifai_api = ClarifaiApi()
+
+
+ClarifaiApi.geometry("1024x950+130+0")
+ClarifaiApi.title("ServiceWhiz.")
+
+def printimage():
+    load = Image.open("hello.jpg")
+    render = IClarifaiApi.PhotoImage(load)
+
+    img = Button(ClarifaiApi, image=render,command=imgpress)
+    img.image = render
+    img.place(x=0,y=0)
+    return;
+
+def imgpress():
+    
+   from ClarifaiApi import Image
+   img = Image.open('picture.jpg')
+   img.show()
+   return;
+
+
