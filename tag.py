36ec23b50ac5418772189fd55c7d708b4a7632d2
Colin Davis <Colin@172-30-26-94.DYNAPOOL.NYU.EDU>
2016-04-02 21:59:20 -0400

    Make method for getting tags in tag.py

--- tag.py
+++ tag.py
@@ -1,11 +1,14 @@
 import sys
 from clarifai.client import ClarifaiApi
-clarifai_api = ClarifaiApi()  # assumes environment variables are set.
-result = clarifai_api.tag_image_urls(sys.argv[1])
-tags = result['results'][0]['result']['tag']['classes']
-#tags = map(str, tags)
-cleanTags = []
-for element in tags:
-	cleanTags += eval(element)
-	
-print(cleanTags)
+
+def getTags(gifUrl):
+	cleanTags = []	
+	clarifai_api = ClarifaiApi()  # assumes environment variables are set.
+	result = clarifai_api.tag_image_urls(gifUrl)
+	tags = result['results'][0]['result']['tag']['classes']
+	#tags = map(str, tags)
+	for element in tags:
+		cleanTags += eval(element)
+		
+	return cleanTags
+
