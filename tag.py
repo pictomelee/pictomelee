e56e7675a4f96f4886190bbb70c22782e44a5ef0
Colin Davis <Colin@172-30-26-94.DYNAPOOL.NYU.EDU>
2016-04-02 21:32:23 -0400

    Added parcing for gif results

--- tag.py
+++ tag.py
@@ -3,5 +3,9 @@ from clarifai.client import ClarifaiApi
 clarifai_api = ClarifaiApi()  # assumes environment variables are set.
 result = clarifai_api.tag_image_urls(sys.argv[1])
 tags = result['results'][0]['result']['tag']['classes']
-tags = map(str, tags)
-print(tags)
+#tags = map(str, tags)
+cleanTags = []
+for element in tags:
+	cleanTags += eval(element)
+	
+print(cleanTags)
