16ea76e657510e6492d2e205cc0d39d403c6b021
crdavis10 <tuf40681@temple.edu>
2016-04-05 01:29:28 -0400

    Delete manage.py

--- manage.py
+++
@@ -1,6 +0,0 @@
-import os
-from application import create_app, db
-from flask.ext.script import Manager, Shell
-
-application = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(application)
